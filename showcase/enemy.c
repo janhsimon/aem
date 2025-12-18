72662ca0c6952b589b111528ded0f00c2fd50806
janhsimon <janhsimon@gmail.com>
2025-12-18 15:59:36 +0100

    Add flinch animation to enemy in showcase app
    This reuses the death animation.

--- showcase/enemy.c
+++ showcase/enemy.c
@@ -55,7 +55,7 @@ static GLuint joint_transform_buffer, joint_transform_texture;
 static mat4* joint_transforms;
 
 static struct AEMAnimationMixer* mixer;
-static struct AEMAnimationChannel* channel;
+static struct AEMAnimationChannel *walk_channel, *flinch_channel;
 
 static mat4 transform;
 static float rot = 0.0f;
@@ -66,16 +66,18 @@ static vec3 hitbox_lower_torso_bottom, hitbox_lower_torso_top;
 
 static float health;
 
-// enum
-//{
-//   EnemyState_WalkingLeft,
-//   EnemyState_WalkingRight
-// } state = EnemyState_WalkingLeft;
-
 static void respawn_enemy(bool play_sound)
 {
+  // Reset health
   health = 100.0f;
 
+  // Reset animation to walking
+  walk_channel->time = 0.0f;
+  walk_channel->animation_index = ENEMY_WALK_ANIMATION_INDEX;
+  walk_channel->playback_speed = 1.75f;
+  walk_channel->is_looping = true;
+
+  // Reset position and angle
   vec3 spawn_position;
   float spawn_yaw;
   get_current_map_random_enemy_spawn(spawn_position, &spawn_yaw);
@@ -84,6 +86,7 @@ static void respawn_enemy(bool play_sound)
                transform); // TODO: This is a hack because the enemy doesn't yet use angles
   glm_scale(transform, (vec3){ 19.25f, 19.25f, 19.25f });
 
+  // Play the optional respawn sound effect
   if (play_sound)
   {
     play_respawn_sound(spawn_position);
@@ -93,8 +96,6 @@ static void respawn_enemy(bool play_sound)
 bool load_enemy(const struct AEMModel* model_)
 {
   model = model_;
-
-  respawn_enemy(false);
 
   glGenBuffers(1, &joint_transform_buffer);
   glGenTextures(1, &joint_transform_texture);
@@ -110,139 +111,152 @@ bool load_enemy(const struct AEMModel* model_)
 
   // Walking
   {
-    channel = aem_get_animation_mixer_channel(mixer, 0);
-    channel->animation_index = ENEMY_WALK_ANIMATION_INDEX;
-    channel->playback_speed = 1.75f;
-    channel->is_playing = true;
+    walk_channel = aem_get_animation_mixer_channel(mixer, 0);
+    walk_channel->animation_index = ENEMY_WALK_ANIMATION_INDEX;
+    walk_channel->playback_speed = 1.75f;
+    walk_channel->is_playing = true;
   }
 
-  joint_transforms = malloc(joint_count * sizeof(mat4));
-  assert(joint_transforms);
-
-  for (uint32_t joint_index = 0; joint_index < joint_count; ++joint_index)
+  // Flinching
   {
-    glm_mat4_identity(joint_transforms[joint_index]);
+    flinch_channel = aem_get_animation_mixer_channel(mixer, 1);
+    flinch_channel->animation_index = ENEMY_DIE_ANIMATION_INDEX;
+    flinch_channel->playback_speed = 1.5f;
+    flinch_channel->is_playing = true;
   }
 
-  glBindBuffer(GL_TEXTURE_BUFFER, joint_transform_buffer);
-  glBufferData(GL_TEXTURE_BUFFER, sizeof(mat4) * joint_count, NULL, GL_DYNAMIC_DRAW);
+  // Enable skeletal animations
+  {
+    joint_transforms = malloc(joint_count * sizeof(mat4));
+    assert(joint_transforms);
 
-  glBindTexture(GL_TEXTURE_BUFFER, joint_transform_texture);
-  glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, joint_transform_buffer);
+    for (uint32_t joint_index = 0; joint_index < joint_count; ++joint_index)
+    {
+      glm_mat4_identity(joint_transforms[joint_index]);
+    }
+
+    glBindBuffer(GL_TEXTURE_BUFFER, joint_transform_buffer);
+    glBufferData(GL_TEXTURE_BUFFER, sizeof(mat4) * joint_count, NULL, GL_DYNAMIC_DRAW);
+
+    glBindTexture(GL_TEXTURE_BUFFER, joint_transform_texture);
+    glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, joint_transform_buffer);
+  }
+
+  respawn_enemy(false);
 
   return true;
 }
 
 void update_enemy(const struct Preferences* preferences, float delta_time)
 {
+  // Update animation
+  aem_update_animation(model, mixer, delta_time, **joint_transforms);
+
   if (health <= 0.0f)
   {
-    aem_update_animation(model, mixer, delta_time, **joint_transforms);
-
-    if (channel->animation_index == ENEMY_DIE_ANIMATION_INDEX)
+    // Handle respawning when the death animation has run out
+    if (walk_channel->animation_index == ENEMY_DIE_ANIMATION_INDEX)
     {
       const float duration = aem_get_model_animation_duration(model, ENEMY_DIE_ANIMATION_INDEX);
-      if (channel->time >= duration)
+      if (walk_channel->time >= duration)
       {
         respawn_enemy(true);
-
-        channel->time = 0.0f;
-        channel->animation_index = ENEMY_WALK_ANIMATION_INDEX;
-        channel->playback_speed = 1.75f;
-        channel->is_looping = true;
       }
     }
   }
   else
   {
-    vec3 old_pos;
-    glm_vec3_copy(transform[3], old_pos);
-
-    // Update position
+    // Handle the hurt animation
+    if (flinch_channel->weight > 0.0f)
     {
-      /*if (state == EnemyState_WalkingLeft)
-      {
-        glm_rotate_y(transform, glm_rad(0.2f), transform);
-      }
-      else if (state == EnemyState_WalkingRight)
-      {
-        glm_rotate_y(transform, glm_rad(-0.2f), transform);
-      }*/
+      const float duration = aem_get_model_animation_duration(model, ENEMY_DIE_ANIMATION_INDEX);
 
-      if (preferences->ai_turning)
-      {
-        glm_rotate_y(transform, glm_rad(rot), transform);
-      }
+      flinch_channel->weight = 1.0f - flinch_channel->time * (3.0f / duration);
 
-      if (preferences->ai_walking)
+      if (flinch_channel->weight <= 0.0f)
       {
-        glm_translate_z(transform, ENEMY_MOVE_SPEED * delta_time);
+        flinch_channel->weight = 0.0f;
       }
     }
-
-    vec3 velocity;
-    glm_vec3_sub(transform[3], old_pos, velocity);
-
-    // Simple collision
+    else
     {
-      const float magnitude = glm_vec3_norm(velocity);
-      const int step_count = (magnitude / ENEMY_COLLIDER_RADIUS) + 1;
-      const float step_length = magnitude / step_count;
-
-      vec3 step_vector;
-      glm_vec3_scale_as(velocity, step_length, step_vector);
-
-      old_pos[1] += ENEMY_COLLIDER_RADIUS; // From feet to capsule bottom center
-
-      for (int step = 0; step < step_count; ++step)
+      // Movement
+      vec3 old_pos, velocity;
       {
-        glm_vec3_add(old_pos, step_vector, old_pos);
+        glm_vec3_copy(transform[3], old_pos);
 
-        vec3 top = { old_pos[0], old_pos[1] + ENEMY_COLLIDER_HEIGHT - ENEMY_COLLIDER_RADIUS * 2, old_pos[2] };
-        collide_capsule(top, old_pos, ENEMY_COLLIDER_RADIUS, CollisionPhase_Walls);
-      }
-
-      old_pos[1] -= ENEMY_COLLIDER_RADIUS; // From capsule bottom center to feet
-      glm_vec3_copy(old_pos, transform[3]);
-    }
-
-    // Update animation
-    aem_update_animation(model, mixer, delta_time, **joint_transforms);
-
-    if (rand() % 100 == 22)
-    {
-      rot = ((rand() % 100) / 100.0f) * ENEMY_TURN_ANGLE - (ENEMY_TURN_ANGLE / 2.0f);
-    }
-
-    // Footstep sounds
-    {
-      static int footstep_counter = 0;
-
-      static float duration = 0.0f;
-      if (duration <= 0.0f)
-      {
-        duration = aem_get_model_animation_duration(model, 1);
-      }
-
-      float relative_time = channel->time / duration;
-      if (relative_time < 0.0f)
-      {
-        relative_time += 1.0f;
-      }
-
-      for (int step_index = 0; step_index < 2; ++step_index)
-      {
-        const float period_start = 0.5f * step_index + 0.25f;
-        if (footstep_counter == step_index && relative_time >= period_start && relative_time < period_start + 0.5f)
+        if (preferences->ai_turning)
         {
-          const int sound_index = (rand() % 2) * 2 + (step_index % 2);
+          glm_rotate_y(transform, glm_rad(rot), transform);
+        }
 
-          vec3 feet;
-          glm_vec3_copy(transform[3], feet);
-          play_enemy_footstep_sound(sound_index, feet);
+        if (preferences->ai_walking)
+        {
+          glm_translate_z(transform, ENEMY_MOVE_SPEED * delta_time);
+        }
 
-          footstep_counter = (footstep_counter + 1) % 2;
-          break;
+        glm_vec3_sub(transform[3], old_pos, velocity);
+      }
+
+      // Simple collision
+      {
+        const float magnitude = glm_vec3_norm(velocity);
+        const int step_count = (magnitude / ENEMY_COLLIDER_RADIUS) + 1;
+        const float step_length = magnitude / step_count;
+
+        vec3 step_vector;
+        glm_vec3_scale_as(velocity, step_length, step_vector);
+
+        old_pos[1] += ENEMY_COLLIDER_RADIUS; // From feet to capsule bottom center
+
+        for (int step = 0; step < step_count; ++step)
+        {
+          glm_vec3_add(old_pos, step_vector, old_pos);
+
+          vec3 top = { old_pos[0], old_pos[1] + ENEMY_COLLIDER_HEIGHT - ENEMY_COLLIDER_RADIUS * 2, old_pos[2] };
+          collide_capsule(top, old_pos, ENEMY_COLLIDER_RADIUS, CollisionPhase_Walls);
+        }
+
+        old_pos[1] -= ENEMY_COLLIDER_RADIUS; // From capsule bottom center to feet
+        glm_vec3_copy(old_pos, transform[3]);
+      }
+
+      // Random turns
+      if (rand() % 100 == 22)
+      {
+        rot = ((rand() % 100) / 100.0f) * ENEMY_TURN_ANGLE - (ENEMY_TURN_ANGLE / 2.0f);
+      }
+
+      // Footstep sounds
+      {
+        static int footstep_counter = 0;
+
+        static float duration = 0.0f;
+        if (duration <= 0.0f)
+        {
+          duration = aem_get_model_animation_duration(model, 1);
+        }
+
+        float relative_time = walk_channel->time / duration;
+        if (relative_time < 0.0f)
+        {
+          relative_time += 1.0f;
+        }
+
+        for (int step_index = 0; step_index < 2; ++step_index)
+        {
+          const float period_start = 0.5f * step_index + 0.25f;
+          if (footstep_counter == step_index && relative_time >= period_start && relative_time < period_start + 0.5f)
+          {
+            const int sound_index = (rand() % 2) * 2 + (step_index % 2);
+
+            vec3 feet;
+            glm_vec3_copy(transform[3], feet);
+            play_enemy_footstep_sound(sound_index, feet);
+
+            footstep_counter = (footstep_counter + 1) % 2;
+            break;
+          }
         }
       }
     }
@@ -374,15 +388,19 @@ void enemy_hurt(const struct Preferences* preferences, float damage, vec3 dir)
 
   if (health <= 0.0f)
   {
-    if (channel->animation_index == ENEMY_DIE_ANIMATION_INDEX || !preferences->ai_death)
+    if (walk_channel->animation_index == ENEMY_DIE_ANIMATION_INDEX || !preferences->ai_death)
     {
       return;
     }
 
-    channel->animation_index = ENEMY_DIE_ANIMATION_INDEX;
-    channel->playback_speed = 1.0f;
-    channel->is_looping = false;
-    channel->time = 0.0f;
+    // Play death animation
+    walk_channel->animation_index = ENEMY_DIE_ANIMATION_INDEX;
+    walk_channel->playback_speed = 1.0f;
+    walk_channel->is_looping = false;
+    walk_channel->time = 0.0f;
+
+    // Make sure the flinch animation doesn't play anymore
+    flinch_channel->weight = 0.0f;
 
     dir[1] = 0.0f; // Flatten
     glm_normalize(dir);
@@ -400,7 +418,9 @@ void enemy_hurt(const struct Preferences* preferences, float damage, vec3 dir)
   }
   else
   {
-  
+    // Play flinch animation
+    flinch_channel->time = 0.0f;
+    flinch_channel->weight = 1.0f;
   }
 }
 
