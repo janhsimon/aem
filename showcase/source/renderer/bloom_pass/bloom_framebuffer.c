8bc7488832f108203312fcf5b87b93b19131b5ce
janhsimon <janhsimon@gmail.com>
2026-06-06 01:49:13 +0200

    Fix visual glitch with bloom in showcase app
    This fixes an issue where bloom would overflow the
    screen edges and wrap around to the opposite side
    of the screen in the showcase app.

--- showcase/source/renderer/bloom_pass/bloom_framebuffer.c
+++ showcase/source/renderer/bloom_pass/bloom_framebuffer.c
@@ -65,6 +65,9 @@ static void create_textures(uint32_t width, uint32_t height)
 
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+      glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, (float[]){ 0.0, 0.0, 0.0, 1.0 });
     }
   }
 
@@ -84,6 +87,9 @@ static void create_textures(uint32_t width, uint32_t height)
 
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+      glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, (float[]){ 0.0, 0.0, 0.0, 1.0 });
     }
   }
 }
